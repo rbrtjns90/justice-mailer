666c89b06cc351c11c4c77040f5ac380a5779daa
Robert Jones <rbrt.jns90@gmail.com>
2019-06-07 20:07:44 -0600

    added whitenoise to middleware

--- colossus/settings.py
+++ colossus/settings.py
@@ -67,6 +67,7 @@ INTERNAL_IPS = [
 MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
