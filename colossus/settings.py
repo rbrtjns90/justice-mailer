a1ec474862d0e0f625c63763ca500b13dc4e0978
Robert Jones <rbrt.jns90@gmail.com>
2019-06-07 19:42:16 -0600

    settings fixed

--- colossus/settings.py
+++ colossus/settings.py
@@ -18,6 +18,7 @@ SECRET_KEY = config('SECRET_KEY', default=string.ascii_letters)
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
+#ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1', cast=Csv())
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1', cast=Csv())
 
 INSTALLED_APPS = [
